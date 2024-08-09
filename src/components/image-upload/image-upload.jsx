4588357edf4f19568c6a3526872583c72e06f740
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-08-10 03:59:30 +0530

    fixing the image upload section

--- src/components/image-upload/image-upload.jsx
+++ src/components/image-upload/image-upload.jsx
@@ -6,21 +6,25 @@ import RemoveSvg from "../globel-components/remove-svg";
 
 function ImageUpload() {
 
-  const [uploadedImages, setUploadedImages] = useState();
+  const [uploadedImage, setuploadedImage] = useState(null);
 
+  
 
   const onDrop = (acceptFiles) => {
-    const allImages = [...acceptFiles, ...uploadedImages];
+  
+   
+    const allImages = [...acceptFiles, ...uploadedImage];
     const imageNames = new Set(
-      uploadedImages.map((img) => img.name.tolowerCase())
+      uploadedImage.map((img) => img.name.toLowerCase())
     );
-
+    
     const newFiles = allImages.filter((file) => {
+      
       const normalizedName = file.name.toLowerCase();
       if (imageNames.has(normalizedName)) {
         return false;
       } else {
-        imageNames.add(normalizedName);
+        imageNames.add(normalizedName);               
         return true;
       }
     })
@@ -28,7 +32,7 @@ function ImageUpload() {
     newFiles.map((file) => Object.assign(file, { preview: URL.createObjectURL(file) }));
 
     if (newFiles?.length) {
-      setUploadedImages((prevFiles) => ({
+      setuploadedImage((prevFiles) => ({
         ...prevFiles,
         images: [...prevFiles.images, ...newFiles],
       }));
@@ -38,17 +42,21 @@ function ImageUpload() {
   }
 
   const { getRootProps, isDragActive } = useDropzone({
+    accept: {
+      'image/*': []
+    },
+    maxSize: 1024 * 1000,
     onDrop,
   });
 
   useEffect(() => {
     return () => {
-      uploadedImages?.forEach((file) => URL.revokeObjectURL(file.preview));
+      uploadedImage?.forEach((file) => URL.revokeObjectURL(file.preview));
     }
-  }, [uploadedImages]);
+  }, [uploadedImage]);
 
   const removeFile = (name) => {
-    setUploadedImages((prev) => {
+    setuploadedImage((prev) => {
       const newImages = prev.images.filter((file) => file.name !== name);
       // if (newImages.length <= 8 && errors.images) {
       //   setErrors((prev) => ({ ...prev, images: nulls }))
@@ -66,7 +74,7 @@ function ImageUpload() {
     <React.Fragment>
     <section className="image-upload-section">
       <span>Add an image</span>
-      <label for="myFileInput">
+      <label {...getRootProps()} for="myFileInput" >
         <div>
           <svg class="w-10 h-10 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"></path>
@@ -76,7 +84,7 @@ function ImageUpload() {
         </div>
 
       </label>
-      <input type="file" id="myFileInput" accept="image/*" />
+      {/* <input  type="file" id="myFileInput" accept="image/*" /> */}
     </section>
     <section className="view-image">
       <RemoveSvg/>
