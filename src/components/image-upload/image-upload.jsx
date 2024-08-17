949d2fe64118e894e389a9ccc5ce8d523332b45a
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-08-17 19:03:43 +0530

    refactor: fixing the upload funtionality

--- src/components/image-upload/image-upload.jsx
+++ src/components/image-upload/image-upload.jsx
@@ -7,7 +7,7 @@ import RemoveSvg from "../globel-components/remove-svg";
 function ImageUpload() {
 
   const [uploadedImage, setuploadedImage] = useState([]);
-  const [rejected,setRejected] = useState([]);
+  const [changeClass,setChangeClass] = useState(false);
 
   const onDrop = useCallback((acceptedFiles,rejectedFiles) => {
   
@@ -30,19 +30,20 @@ function ImageUpload() {
   
   console.log(acceptedFiles);
   if (acceptedFiles?.length) {
-    setuploadedImage(previousFiles => [
-      ...previousFiles,
-      ...acceptedFiles.map(file =>
-        Object.assign(file, { preview: URL.createObjectURL(file) })
-      )
-    ])
+
+    if(acceptedFiles?.length===1){
+      setuploadedImage(previousFiles => [
+        ...previousFiles,
+        ...acceptedFiles.map(file =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        )
+      ]);
+    }
+
+    setChangeClass(true);
   }
 
-
-
-  if (rejectedFiles?.length) {
-    setRejected(previousFiles => [...previousFiles, ...rejectedFiles])
-  }
+  console.log(acceptedFiles.length);
     
   },[])
 
@@ -71,15 +72,21 @@ function ImageUpload() {
     //     images: newImages,
     //   };
     // });
-    setuploadedImage((prev) => prev.filter((file) => file.name !== name));
+    setuploadedImage((prev) => prev.filter((file) => file.name !== name),setChangeClass(false));
   }
+
+
+
+
+
 
   return (
     <React.Fragment>
+      <form action="">
     <section className="image-upload-section">
       <span>Add an image</span>
-      <label {...getRootProps()} for="myFileInput" >
-      <input {...getInputProps()} />
+      <label {...getRootProps()} for="myFileInput">
+      <input {...getInputProps()}/>
         <div>
           <svg class="w-10 h-10 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"></path>
@@ -91,15 +98,17 @@ function ImageUpload() {
       </label>
       {/* <input  type="file" id="myFileInput" accept="image/*" /> */}
     </section>
-    <section className="view-image">
+    <section className={changeClass ? "view-image" : "view-imageOn"}>
     {uploadedImage.map(file=>(
       <>
       <RemoveSvg onRemove={()=>removeFile(file.name)}/>
+    {/* <button type="submit">Submit</button> */}
       <img key={file.name} className="detecting-image" src={file.preview} alt={file.name} onLoad={() => {
         URL.revokeObjectURL(file.preview);
       } } /></>
     ))}
     </section>
+    </form>
     </React.Fragment>
   )
 }
